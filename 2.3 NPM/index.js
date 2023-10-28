5ace7682ddadd2e1a2e6a693c8d648bcba3c84b7
Louis Nguyen <ndmn004@gmail.com>
2023-10-28 15:36:14 +1100

    Initial upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import generateName from 'sillyname';
+const sillyname = generateName();
+console.log(`My name is ${sillyname}`);
+
+import superheroes from 'superheroes';
+const name = superheroes.random();
+console.log(`I am ${name}!`);
